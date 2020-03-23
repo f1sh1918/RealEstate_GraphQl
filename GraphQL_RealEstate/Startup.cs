9d5969a15e29aa0da334ff6b9a26f9f87e5643b4
afischer <andreas.fischer@prodyna.com>
2020-03-23 16:29:03 +0100

    Init correct projects .netcore

--- GraphQL_RealEstate/Startup.cs
+++ GraphQL_RealEstate/Startup.cs
@@ -8,8 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RealEstateManager.Database2;
 
-namespace GraphQL_RealEstate
+namespace RealEstateManager.Api
 {
     public class Startup
     {
@@ -23,34 +24,17 @@ namespace GraphQL_RealEstate
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RealEstateContext db)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
         }
     }
 }
---
+++ RealEstateManager.Database/RealEstateContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Design;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace RealEstateManager.Database
+{
+    public class RealEstateContextFactory: IDesignTimeDbContextFactory<RealEstateContext>
+    {
+        public RealEstateContext CreateDbContext(string[] args)
+        {
+            var configuration = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<RealEstateContext>();
+            var connectionString = configuration.GetConnectionString("RealEstateDb");
+            builder.UseSqlServer(connectionString);
+
+            return new RealEstateContext(builder.Options);
+
+        }
+        
+    }
+}
